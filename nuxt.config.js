fde25bfcf64cef0b00f0abe1f771619fa7ff661f
tsunose <tsunose.developer@gmail.com>
2020-05-13 16:36:42 +0900

    set up contentful

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: process.env.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
+  },
   mode: 'universal',
   /*
    ** Headers of the page
